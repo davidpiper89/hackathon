2478509af058bfec0d36df88c874550200eb3325
paula cavender <you@example.com>
2022-12-16 15:03:44 +0000

    background

--- src/components/letterPages/S/Audio.jsx
+++ src/components/letterPages/S/Audio.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 const Audio = () => {
-  return <div><FontAwesomeIcon icon="fa-solid fa-play" /></div>;
+  return (
+    <div>
+      <FontAwesomeIcon icon={faPlay} />
+    </div>
+  );
 };
 
 export default Audio;
