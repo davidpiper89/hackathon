bb768e58e333204dc229e1740e31f5df137f5dfd
daniel wong <kappachino2137211@gmail.com>
2022-12-16 20:19:15 +0800

    Added routes

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import ".//HomeStyling.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className="homepage-header-container">
+        <h1 className="homepage-header">FT-2 Phonics-tuition</h1>
+      </div>
+      <div className="homepage-content-container">
+        <div className="linkContainer">
+          <Link className="links">Start</Link>
+        </div>
+        <div className="linkContainer">
+          <Link to="/Help" className="links">
+            Help
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
