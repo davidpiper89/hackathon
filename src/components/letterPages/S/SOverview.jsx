7bc031dc2b5a168c41ce973d2aee634f1bb422ca
daniel wong <kappachino2137211@gmail.com>
2022-12-16 23:53:52 +0800

    added css

--- src/components/letterPages/S/SOverview.jsx
+++ src/components/letterPages/S/SOverview.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import Letter from "./Letter";
 import Audio from "./Audio";
-import LetterImage from "./LetterImage";
 import GuessingImages from "./GuessingImages";
 import Next from "./Next";
 import "../Letters.css";
 
 const Overview = () => {
   return (
-    <div className="container">
-      <div className="header">
-        <Letter />
+    <>
+      <div className="game-container">
+        <div className="header">
+          <Letter />
 
-        <Audio />
+          <Audio />
+        </div>
+        <div className="body">
+          <GuessingImages />
+        </div>
+        <div className="footer">{/* <Next />  */}</div>
       </div>
-      <div className="body">
-        <LetterImage />
-
-        <GuessingImages />
-      </div>
-      <div className="footer">{/* <Next />  */}</div>
-    </div>
+    </>
   );
 };
 
