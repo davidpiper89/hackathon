6e225437970962113d868fed2630130d79afc8d3
davidpiper89 <davidpiper1989@gmail.com>
2022-12-16 13:47:44 +0000

    images

---
+++ src/components/letterPages/A/Next.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Next = () => {
+  return <div></div>;
+};
+
+export default Next;
--- src/components/letterPages/S/GuessingImages.jsx
+++ src/components/letterPages/S/GuessingImages.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React from "react";
+import snake1 from "./snake-pic.png";
+import snake2 from "./snake-pic.png";
+import snake3 from "./snake-pic.png";
+
 const GuessingImages = () => {
-    return <></>;
+
+const wrong = () => {
+  
 }
- 
+
+  return (
+    <>
+      <div className="openWrong">
+        <img src={snake1} alt="no work" width="100px" onClick={() => wrong}></img>
+      </div>
+      <div className="openWrong">
+        <img src={snake2} alt="no work" width="100px"></img>
+      </div>
+      <div className="openRight">
+        <img src={snake3} alt="no work" width="100px"></img>
+      </div>
+    </>
+  );
+};
+
 export default GuessingImages;
